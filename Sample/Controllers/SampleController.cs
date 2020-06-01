cd36aa73e6633dea1e7a7ad371130100a9497a8d
Fatih Sever <fatih@fatihsever.com>
2020-06-01 16:19:51 +0300

    Clean-up

---
+++ Sample/Controllers/SampleController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using PrimeAppsDotNet;
+
+namespace Sample.Controllers
+{
+    public class SampleController : BaseController
+    {
+        private IConfiguration _configuration;
+        private IMemoryCache _memoryCache;
+
+        public SampleController(IConfiguration configuration, IMemoryCache memoryCache)
+        {
+            _configuration = configuration;
+            _memoryCache = memoryCache;
+        }
+
+        public async Task<IActionResult> GetAllUsers()
+        {
+            using (var primeapps = new PrimeApps(_configuration, _memoryCache))
+            {
+                primeapps.TenantId = TenantId;
+
+                var record = await primeapps.GetAllUsers();
+
+                return Ok(record);
+            }
+        }
+    }
+}
