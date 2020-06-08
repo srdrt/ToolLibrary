ad71d333833a101f8c0255065e4eaeaac0baaa1e
Onur Bostancıoğlu <onur.bostancioglu@primeapps.io>
2020-06-08 10:47:40 +0300

    JsonProperty has been added for User  model.

--- Sample/Models/User.cs
+++ Sample/Models/User.cs
@@ -1,11 +1,20 @@
+using Newtonsoft.Json;
+
 namespace Sample.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        [JsonProperty("first_name")]
         public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
         public string LastName { get; set; }
+
+        [JsonProperty("full_name")]
         public string FullName { get; set; }
+
         public string Email { get; set; }
     }
 }
